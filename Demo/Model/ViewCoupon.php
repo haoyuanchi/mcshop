f4a36f4fe94d49ca849ef6389e2aa9b5018823e5
hychi <chihaoyuan6@163.com>
2016-07-04 14:59:17 +0800

    增加地址接口

--- Demo/Model/ViewCoupon.php
+++ Demo/Model/ViewCoupon.php
@@ -12,12 +12,12 @@ class Model_ViewCoupon extends PhalApi_Model_NotORM {
     }
 
     public function getListByUserId($userId, $isUsed, $isExpired){
-        $row = $this->getORM()
+        $rows = $this->getORM()
             ->select('*')
             ->where('member_id', $userId)
             ->where('is_used', $isUsed)
             ->where('is_expired', $isExpired)
-            ->fetch();
-        return $row;
+            ->fetchAll();
+        return $rows;
     }
 }
