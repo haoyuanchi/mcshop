d96da7b6cd79bb211092229c7a6563dc8ef06d15
hychi <chihaoyuan6@163.com>
2016-07-28 00:53:48 +0800

    1. 支付测试

--- Public/init.php
+++ Public/init.php
@@ -13,15 +13,15 @@ require_once API_ROOT . '/PhalApi/PhalApi.php';
 $loader = new PhalApi_Loader(API_ROOT, 'Library');
 
 // 支持跨域请求
-//header("Access-Control-Allow-Origin", "*");
+/*//header("Access-Control-Allow-Origin", "*");
 header('Access-Control-Allow-Origin:*');
 /*header("Access-Control-Allow-Methods", "POST,OPTIONS,GET");
 header('Access-Control-Allow-Headers:x-requested-with,content-type');*/
 
 /*header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);*/
-header('Access-Control-Allow-Methods: "OPTIONS, GET, POST"');
+/*header('Access-Control-Allow-Methods: "OPTIONS, GET, POST"');
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Headers: "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"');
+header('Access-Control-Allow-Headers: "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"');*/
 
 /** ---------------- 注册&初始化 基本服务组件 ---------------- **/
 
