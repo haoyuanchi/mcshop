b07ccb7901926c29a77a3b52e0c4525e942cbb13
hychi <chihaoyuan6@163.com>
2016-07-29 20:06:28 +0800

    1. 解决跨域问题

--- PhalApi/PhalApi.php
+++ PhalApi/PhalApi.php
@@ -49,14 +49,6 @@ class PhalApi {
     public function response() {
         $rs = DI()->response;
 
-        // 解决跨域问题
-        //header('content-type: application:json; charset=utf-8');
-        header('Access-Control-Allow-Origin: *');
-        //header("Access-Control-Allow-Credentials: true");
-        header('Access-Control-Allow-Headers: X-Requested-With');
-        header('Access-Control-Allow-Headers: Content-Type');
-        //header('Access-Control-Allow-Methods: OPTIONS, GET, POST');
-
         try {
             $api = PhalApi_ApiFactory::generateService(); 
 
